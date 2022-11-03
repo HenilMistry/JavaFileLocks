33c38aaf26ec111155795acf817cb0df4949fb14
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2022-11-03 22:23:03 +0530

    added a class to test the Locks package

--- src/TestPackage.java
+++ src/TestPackage.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class TestPackage {
+import Locks.Exceptions.NotLockedException;
+import Locks.ExclusiveLock;
+import Locks.SharedLock;
+
+public class TestPackage {
+    public static void main(String[] args) throws NotLockedException {
+        SharedLock SLock = new SharedLock("Files\\","MySelf.txt");
+        ExclusiveLock XLock = new ExclusiveLock("Files\\","MySelf.txt");
+
+        SLock.acquireLock();
+        SLock.readWholeFile(true);
+        // this will throw OverlappingFileLockException...
+        // XLock.writeToFile("This is amazing!");
+        SLock.releaseLock();
+        XLock.acquireLock(true);
+        XLock.appendToFile("\nThis is amazing right...?");
+        XLock.releaseLock();
+    }
 }
