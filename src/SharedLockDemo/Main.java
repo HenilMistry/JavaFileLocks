db8f5f7618b35158024c2d6670473ec9e70e48bd
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2022-11-03 22:22:33 +0530

    made a package for demonstration of multiple thread acquiring lock on same file

--- src/SharedLockDemo/Main.java
+++ src/SharedLockDemo/Main.java
@@ -1,2 +1,17 @@
-package SharedLockDemo;public class Main {
+package SharedLockDemo;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        ThreadDemo reader1 = new ThreadDemo("Reader1");
+        ThreadDemo reader2 = new ThreadDemo("Reader2");
+        ThreadDemo reader3 = new ThreadDemo("Reader3");
+
+        reader1.start();
+        reader1.join();
+        reader2.start();
+        reader2.join();
+        reader3.start();
+
+    }
 }
--- src/SharedLockDemo/ThreadDemo.java
+++ src/SharedLockDemo/ThreadDemo.java
@@ -1,4 +1,45 @@
 package SharedLockDemo;
 
-public class ThreadDemo {
+import Locks.Exceptions.NotLockedException;
+import Locks.SharedLock;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ThreadDemo extends Thread {
+
+    public ThreadDemo(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        // created object of shared lock...
+        SharedLock SLock = new SharedLock("Files\\","MySelf.txt");
+        // acquired lock...
+        SLock.acquireLock();
+
+        // reading from file...
+        BufferedReader scanner = SLock.getReader();
+        try {
+            String data = scanner.readLine();
+            while (data!=null) {
+                System.out.println("From thread : "+this.getName()+" >> "+data);
+                Thread.sleep(1000);
+                data = scanner.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // releasing the lock...
+        try {
+            SLock.releaseLock();
+        } catch (NotLockedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
